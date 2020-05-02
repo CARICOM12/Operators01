4bd10ecffdc09811b2e94fd0eee4eec42a4cc5e4
CARICOM12 <0190a@ukr.net>
2020-05-02 21:44:19 +0300

    Add files via upload

---
+++ Operators01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Operators01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int result, remainder;
+            result = 10 / 3;
+            remainder = 10 % 3;
+            Console.WriteLine(result, remainder);
+            //Why in the output it gives 4 instead of two numbers
+        }
+    }
+}
